b6db7a88e5358a1db2cc9ff420ebe429ef03618f
lpiotrkowski <lipek92@gmail.com>
2015-03-28 18:07:58 -0400

    dodatkowe ikonki

--- src/main/java/chat/other/Emoticons.java
+++ src/main/java/chat/other/Emoticons.java
@@ -9,8 +9,17 @@ public class Emoticons {
 
     public Emoticons()
     {
-        emoticons.put(":)", "<img src=\"../VAADIN/themes/mytheme/emoticons/smile.png\">");
         emoticons.put(":D", "<img src=\"../VAADIN/themes/mytheme/emoticons/big-smile.png\">");
+        emoticons.put(":d", "<img src=\"../VAADIN/themes/mytheme/emoticons/big-smile.png\">");
+        emoticons.put(";(", "<img src=\"../VAADIN/themes/mytheme/emoticons/cry.png\">");
+        emoticons.put(":*", "<img src=\"../VAADIN/themes/mytheme/emoticons/kiss.png\">");
+        emoticons.put(":(", "<img src=\"../VAADIN/themes/mytheme/emoticons/sad.png\">");
+        emoticons.put(":)", "<img src=\"../VAADIN/themes/mytheme/emoticons/smile.png\">");
+        emoticons.put(":o", "<img src=\"../VAADIN/themes/mytheme/emoticons/surprised.png\">");
+        emoticons.put(":O", "<img src=\"../VAADIN/themes/mytheme/emoticons/surprised.png\">");
+        emoticons.put(":p", "<img src=\"../VAADIN/themes/mytheme/emoticons/tongue.png\">");
+        emoticons.put(":P", "<img src=\"../VAADIN/themes/mytheme/emoticons/tongue.png\">");
+        emoticons.put(";)", "<img src=\"../VAADIN/themes/mytheme/emoticons/wink.png\">");
     }
 
     public String replaceEmots(String message)
