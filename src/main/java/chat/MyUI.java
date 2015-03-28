58850c9c2544ae9fc05ee1ee974f53a2031a16c4
lpiotrkowski <lipek92@gmail.com>
2015-03-27 20:01:41 -0400

    logowanie

--- src/main/java/chat/MyUI.java
+++ src/main/java/chat/MyUI.java
@@ -2,16 +2,16 @@ package chat;
 
 import javax.servlet.annotation.WebServlet;
 
+import chat.views.MainView;
+import chat.views.LoginView;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
+import com.vaadin.navigator.Navigator;
+import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
 
 /**
  *
@@ -22,18 +22,55 @@ public class MyUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        layout.setMargin(true);
-        setContent(layout);
+        //
+        // Create a new instance of the navigator. The navigator will attach
+        // itself automatically to this view.
+        //
+        new Navigator(this, this);
 
-        Button button = new Button("Click Me");
-        button.addClickListener(new Button.ClickListener() {
+        //
+        // The initial log view where the user can login to the application
+        //
+        getNavigator().addView(LoginView.NAME, LoginView.class);//
+
+        //
+        // Add the main view of the application
+        //
+        getNavigator().addView(MainView.NAME, MainView.class);
+
+        //
+        // We use a view change handler to ensure the user is always redirected
+        // to the login view if the user is not logged in.
+        //
+        getNavigator().addViewChangeListener(new ViewChangeListener() {
+
             @Override
-            public void buttonClick(ClickEvent event) {
-                layout.addComponent(new Label("Thank you for clicking"));
+            public boolean beforeViewChange(ViewChangeEvent event) {
+
+                // Check if a user has logged in
+                boolean isLoggedIn = getSession().getAttribute("user") != null;
+                boolean isLoginView = event.getNewView() instanceof LoginView;
+
+                if (!isLoggedIn && !isLoginView) {
+                    // Redirect to login view always if a user has not yet
+                    // logged in
+                    getNavigator().navigateTo(LoginView.NAME);
+                    return false;
+
+                } else if (isLoggedIn && isLoginView) {
+                    // If someone tries to access to login view while logged in,
+                    // then cancel
+                    return false;
+                }
+
+                return true;
+            }
+
+            @Override
+            public void afterViewChange(ViewChangeEvent event) {
+
             }
         });
-        layout.addComponent(button);
 
     }
 
